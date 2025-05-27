a7aa501611d3d9d1ad59d8433a8b7fc12e12216d
Messy Ghost <39986054+MessyGhost@users.noreply.github.com>
2025-05-27 16:03:22 +0800

    rename: rename boss to nightmare of corruption
    - also delete old code

---
+++ Content/NPCs/NightmareCorruption/NightmareCorruptionPhantom.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AshenVoid.Content.NPCs.NightmareCorruption
+{
+    public class NightmareCorruptionPhantom : ModNPC
+    {
+        public override string Texture => "AshenVoid/Content/NPCs/NightmareCorruption/NightmareOfCorruption.png";
+
+        public override void SetDefaults()
+        {
+            NPC.width = 100;
+            NPC.height = 100;
+            NPC.lifeMax = 9999;
+            NPC.damage = 50;
+            NPC.defense = 30;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0f;
+            NPC.noGravity = true;
+            NPC.noTileCollide = true;
+            NPC.dontTakeDamage = true;
+            NPC.alpha = 80;
+            NPC.value = 0f;
+        }
+    }
+}
---
+++ Content/NPCs/NightmareCorruption/NightmareOfCorruption.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using static Terraria.GameContent.Animations.On_Actions;
+
+namespace AshenVoid.Content.NPCs.NightmareCorruption
+{
+    public class NightmareOfCorruption : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[NPC.type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 230;
+            NPC.height = 180;
+            NPC.lifeMax = 13100;
+            NPC.damage = 52;
+            NPC.defense = 10;
+            NPC.knockBackResist = 0f;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.value = Item.buyPrice(0, 3, 0, 0);
+            NPC.boss = true;
+            NPC.noGravity = true;
+            NPC.noTileCollide = true;
+            NPC.netAlways = true;
+            NPC.aiAction = -1;
+            NPC.alpha = 255;
+        }
+    }
+}
