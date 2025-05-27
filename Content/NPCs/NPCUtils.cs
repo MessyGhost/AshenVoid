c4ce3148f5876d2af001b373413e1af367adedfb
Messy Ghost <39986054+MessyGhost@users.noreply.github.com>
2025-05-27 16:27:11 +0800

    feat: NPC utils
    - to sync NPC

---
+++ Content/NPCs/NPCUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+
+namespace AshenVoid.Content.NPCs
+{
+    internal class NPCUtils
+    {
+        public static void ForceSyncNPC(int npcId)
+        {
+            if (Main.netMode == NetmodeID.Server)
+            {
+                NetMessage.SendData(MessageID.SyncNPC, -1, -1, null, npcId);
+            }
+        }
+    }
+}
